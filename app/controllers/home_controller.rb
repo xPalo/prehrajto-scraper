dd8d9e48834e345833cc95588ecb31e47959267f
adam.palo <adam.palo@muziker.com>
2022-07-29 14:18:52 +0200

    added Formula

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,21 @@
 class HomeController < ApplicationController
 
-  def prehrajto
+  def formula
 
-    require "faraday"
-    require "faraday/net_http"
-    Faraday.default_adapter = :net_http
+  end
+
+  def prehrajto
 
     if params[:search_url] && params[:search_url].length > 0
 
       @divs = Array.new
       params[:search_url] = params[:search_url][8..-1]
-      unparsed_page = Faraday.get("https://prehrajto.cz/hledej/#{CGI.escape(params[:search_url].to_s)}")
+      url = "https://prehrajto.cz/hledej/#{CGI.escape(params[:search_url].to_s)}"
+      unparsed_page = HTTParty.get(url)
 
       unless unparsed_page.body.nil?
 
-        parsed_page = Nokogiri::HTML(unparsed_page.body)
+        parsed_page = Nokogiri::HTML(unparsed_page.force_encoding('UTF-8'))
         result_divs = parsed_page.css("section").css("div.column")
 
         result_divs.each { |r|
@@ -30,38 +31,6 @@ class HomeController < ApplicationController
       end
     end
 
-    # if params[:search_url] && params[:search_url].length > 0
-    #
-    #   @divs = Array.new
-    #   params[:search_url] = params[:search_url][8..-1]
-    #   url = "https://prehrajto.cz/hledej/#{CGI.escape(params[:search_url].to_s)}"
-    #   unparsed_page = HTTParty.get(url)
-    #
-    #   unless unparsed_page.body.nil?
-    #
-    #     puts "URL = #{url}"
-    #
-    #     parsed_page = Nokogiri::HTML(unparsed_page.force_encoding('UTF-8'))
-    #     parsed_xml = Nokogiri::XML(unparsed_page)
-    #
-    #     result_divs = parsed_page.css("section").css("div.column")
-    #
-    #     @check = unparsed_page.body
-    #     puts "ERRORS: #{parsed_page.errors}"
-    #
-    #     result_divs.each { |r|
-    #       div = {
-    #         "href" => r.css("a")[0].attributes["href"].value.strip.to_s,
-    #         "image_src" => r.css("img")[0].attributes["src"].value.strip.to_s,
-    #         "duration" => r.css("strong.video-item-info-time").text.strip.to_s,
-    #         "size" => r.css("strong.video-item-info-size").text.strip.to_s,
-    #         "title" => r.css("h2.video-item-title").text.strip.to_s
-    #       }
-    #       @divs << div
-    #     }
-    #   end
-    # end
-
     if params[:movie_url] && params[:movie_url].length > 0
 
       url = "https://prehrajto.cz/#{params[:movie_url]}"
@@ -69,9 +38,6 @@ class HomeController < ApplicationController
 
       unless unparsed_page.body.nil?
         parsed_page = Nokogiri::HTML(unparsed_page.force_encoding('UTF-8'))
-
-        puts "ERRORS: #{parsed_page.errors}"
-
         storage_substring = parsed_page.to_s[parsed_page.to_s.index('var sources')..parsed_page.to_s.index('var tracks')]
         @video_src = storage_substring[/#{"\""}(.*?)#{"\""}/m, 1].to_s
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "home#prehrajto"
   match "lang/:locale", to: "home#change_locale", as: :change_locale, via: [:get]
+  get "/formula", to: "home#formula"
 end
