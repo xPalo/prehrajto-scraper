3b62464cfe38e053c7638188b8ae052f36352f62
Adam Paľo <adam.palo@muziker.com>
2025-08-03 00:14:03 +0200

    Remove show

--- app/controllers/watchdogs_controller.rb
+++ app/controllers/watchdogs_controller.rb
@@ -3,14 +3,11 @@ require "net/http"
 
 class WatchdogsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_watchdog, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_watchdog, only: [:edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
 
   def index
     @watchdogs = current_user.watchdogs.order(is_active: :desc, id: :asc)
-  end
-
-  def show
   end
 
   def new
@@ -26,10 +23,8 @@ class WatchdogsController < ApplicationController
     respond_to do |format|
       if @watchdog.save
         format.html { redirect_to watchdogs_url, notice: t(:'watchdog.created') }
-        format.json { render :show, status: :created, location: @watchdog }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @watchdog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -38,10 +33,8 @@ class WatchdogsController < ApplicationController
     respond_to do |format|
       if @watchdog.update(watchdog_params)
         format.html { redirect_to watchdogs_url, notice: t(:'watchdog.updated') }
-        format.json { render :show, status: :created, location: @watchdog }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @watchdog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -51,7 +44,6 @@ class WatchdogsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to watchdogs_url, notice: t(:'watchdog.deleted') }
-      format.json { head :no_content }
     end
   end
 
