b9d1c78ebdaa85f9460c9f69eeefbe22c7db312c
Adam Paľo <adam.palo@muziker.com>
2025-08-03 22:33:02 +0200

    Improve UX

--- app/models/watchdog.rb
+++ app/models/watchdog.rb
@@ -3,4 +3,11 @@ class Watchdog < ApplicationRecord
 
   validates :from_airport, presence: true
   validates :user_id, presence: true
+  validate :dates_valid
+
+  def dates_valid
+    return if date_watch_from <= date_watch_to
+
+    errors.add(:date_watch_from, :invalid)
+  end
 end
--- pyservice/ryanair_fetch.py
+++ pyservice/ryanair_fetch.py
@@ -1,15 +1,8 @@
 import sys
 import json
+import argparse
 from datetime import datetime
 from ryanair import Ryanair
-
-# airport_origin = sys.argv[1]
-# date_from = sys.argv[2].strptime(date_from_str, "%Y-%m-%d").date()
-# date_to = sys.argv[3].strptime(date_from_str, "%Y-%m-%d").date()
-# destination_country = sys.argv[4]
-# departure_time_from = sys.argv[5].strptime(date_from_str, "%Y-%m-%d").date()
-# departure_time_to = sys.argv[6].strptime(date_from_str, "%Y-%m-%d").date()
-# destination_airport = sys.argv[7]
 
 parser = argparse.ArgumentParser(description="Fetch Ryanair flights")
 parser.add_argument('--from', dest='airport_origin', required=True, help='Origin airport (e.g. VLC)')
@@ -22,41 +15,39 @@ parser.add_argument('--to-airport', help='Destination airport (e.g. VIE)')
 
 args = parser.parse_args()
 
-date_from = datetime.strptime(args.date_from, "%Y-%m-%d").date()
-date_to = datetime.strptime(args.date_to, "%Y-%m-%d").date()
+kwargs = {
+    'airport': args.airport_origin,
+    'date_from': datetime.strptime(args.date_from, "%Y-%m-%d").date(),
+    'date_to': datetime.strptime(args.date_to, "%Y-%m-%d").date()
+}
 
-departure_time_from = (
-    datetime.strptime(args.departure_time_from, "%Y-%m-%d").date()
-    if args.departure_time_from else None
-)
-departure_time_to = (
-    datetime.strptime(args.departure_time_to, "%Y-%m-%d").date()
-    if args.departure_time_to else None
-)
+if args.to_country:
+    kwargs['destination_country'] = args.to_country
+
+if args.departure_time_from:
+    kwargs['departure_time_from'] = datetime.strptime(args.departure_time_from, "%Y-%m-%d").date()
+
+if args.departure_time_to:
+    kwargs['departure_time_to'] = datetime.strptime(args.departure_time_to, "%Y-%m-%d").date()
+
+if args.to_airport:
+    kwargs['destination_airport'] = args.to_airport
 
 api = Ryanair()
-flights = api.get_cheapest_flights(
-    airport=args.airport_origin,
-    date_from=date_from,
-    date_to=date_to,
-    destination_country=args.to_country,
-    departure_time_from=departure_time_from,
-    departure_time_to=departure_time_to,
-    destination_airport=args.to_airport
-)
+flights = api.get_cheapest_flights(**kwargs)
 
 output = []
 for f in flights:
     output.append({
         "departure": f.departureTime.isoformat(),
-	    "flight_number": f.flightNumber,
+        "flight_number": f.flightNumber,
         "price": f.price,
-	    "currency": f.currency,
+        "currency": f.currency,
         "origin": f.origin,
-	    "originFull": f.originFull,
+        "originFull": f.originFull,
         "destination": f.destination,
-	    "destinationFull": f.destinationFull,
-	    "airline": "Ryanair"
+        "destinationFull": f.destinationFull,
+        "airline": "Ryanair"
     })
 
 print(json.dumps(output))
